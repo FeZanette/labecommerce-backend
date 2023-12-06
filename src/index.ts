4eaacb6d1895f74ccc67dd6fcf5f9d025659f2da
FeZanette <fernandazanette1975@gmail.com>
2023-12-06 15:05:20 -0300

    Apis e Express finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,119 @@
 // import { users, products, getAllProducts, searchProductByName, createProduct } from "./database"
-import { createProduct, createUser, getAllProducts, getAllUsers, searchProductByName } from "./database"
+import {
+  createProduct,
+  createUser,
+  getAllProducts,
+  getAllUsers,
+  products,
+  searchProductByName,
+  users,
+} from "./database";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { TProduct, TUser } from "./types";
+import { Tracing } from "trace_events";
 
-// console.log(users);
-// console.log(products);
+const app = express();
 
-// --------------------------------------------------------
-// EXERCÍCIO 1:
+app.use(express.json());
+app.use(cors());
 
-const createUserResult = createUser ("u003", "Astrodev", "astrodev@email.com", "astrodev99")
-console.log(createUserResult);
+app.listen(3003, () => {
+  console.log("Servidor rodando na porta 3003");
+});
 
-const allUsers = getAllUsers()
-console.log(`Lista de usuários:`, allUsers);
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Pong!");
+});
+
+// Get All Users
+app.get("/users", (req: Request, res: Response) => {
+  res.send(users);
+});
+
+// Get All Products
+// app.get("/products", (req: Request, res: Response) => {
+//     res.send(products)
+// })
+
+// Get All Products com query
+app.get("/products", (req: Request, res: Response) => {
+  const nameToFind = req.query.name as string;
+  console.log(nameToFind);
+
+  if (nameToFind) {
+    const result: TProduct[] = products.filter((product) => {
+      return product.name.toLowerCase().includes(nameToFind.toLowerCase());
+    });
+    res.status(200).send(result);
+  } else {
+    res.status(200).send(products);
+  }
+});
+
+// Create User
+app.post("/users", (req: Request, res: Response) => {
+  const id = req.body.id as string;
+  const name = req.body.name as string;
+  const email = req.body.email as string;
+  const password = req.body.password as string;
+
+  const newUser = {
+    id,
+    name,
+    email,
+    password,
+    createdAt: new Date().toISOString(),
+  };
+
+  users.push(newUser)
+  res.status(201).send("Cadastro realizado com sucesso");
+});
+
+// Create Product
+app.post("/products", (req: Request, res: Response) => {
+  const id = req.body.id as string;
+  const name = req.body.name as string;
+  const price = req.body.pricce as number;
+  const description = req.body.description as string;
+  const imageUrl = req.body.imageUrl as string
+
+  const newProduct = {
+    id,
+    name,
+    price,
+    description,
+    imageUrl
+  };
+
+  products.push(newProduct)
+  res.status(201).send("Produto cadastrado com sucesso");
+});
 
 
-// --------------------------------------------------------
-// EXERCÍCIO 2:
+// // console.log(users);
+// // console.log(products);
 
-const createProductResult = createProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "https://images.unsplash.com/photo")
-console.log(createProductResult);
+// // --------------------------------------------------------
+// // EXERCÍCIO 1:
 
-const allProducts = getAllProducts()
-console.log(`Lista de produtos:`, allProducts);
+// const createUserResult = createUser ("u003", "Astrodev", "astrodev@email.com", "astrodev99")
+// console.log(createUserResult);
 
-// --------------------------------------------------------
-// EXERCÍCIO 3:
+// const allUsers = getAllUsers()
+// console.log(`Lista de usuários:`, allUsers);
 
-const searchResults = searchProductByName("Monitor")
-console.log(`O produto buscado é:`,searchResults);
+// // --------------------------------------------------------
+// // EXERCÍCIO 2:
+
+// const createProductResult = createProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "https://images.unsplash.com/photo")
+// console.log(createProductResult);
+
+// const allProducts = getAllProducts()
+// console.log(`Lista de produtos:`, allProducts);
+
+// // --------------------------------------------------------
+// // EXERCÍCIO 3:
+
+// const searchResults = searchProductByName("Monitor")
+// console.log(`O produto buscado é:`,searchResults);
