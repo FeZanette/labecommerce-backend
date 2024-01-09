34ae39ac5c74617619c953a736fe47698041cb42
FeZanette <fernandazanette1975@gmail.com>
2024-01-09 20:14:28 -0300

    Relações SQL I finalizada

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -62,3 +62,31 @@ SET
     description = 'Monitor Led Full HD 24 polegadas alterado',
     image_url = 'https://www.kabum.com.br/busca/mouse-redragon'
 WHERE id = 'p002';
+
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    created_at TEXT NOT NULL TIMESTAMP,
+    FOREIGN KEY (buyer) REFERENCES users(id);
+)
+
+INSERT INTO purchases (id, buyer, total_price, created_at)
+VALUES
+    ('p001', 'u001', 299.99, CURRENT_TIMESTAMP),
+    ('p002', 'u002', 99.99, CURRENT_TIMESTAMP);
+
+UPDATE purchases
+SET total_price = 1299.99
+WHERE id = 'p001';
+
+SELECT
+    purchases.id,
+    purchases.buyer AS buyer_id,
+    users.name AS buyer_name,
+    users.email,
+    purchases.total_price,
+    purchases.created_at
+FROM purchases
+INNER JOIN users
+ON purchases.buyer = users.id;
